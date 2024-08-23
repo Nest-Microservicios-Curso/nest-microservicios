82eb82d6914f27f942bd98b0760f63fdd7b19275
Jeroham Sanchez <jeroham.sanchez@gmail.com>
2024-08-23 15:52:31 -0500

    S2-14: joi - esquema de validacion

--- app-refuerzo/src/config/envs.ts
+++ app-refuerzo/src/config/envs.ts
@@ -1,7 +1,24 @@
 import 'dotenv/config';
+import * as joi from 'joi';
 
-// TODO: VALIDACIÓN USANDO ESQUEMAS
+interface EnvVariables {
+  PORT: number;
+}
+
+const envsSchema = joi
+  .object({
+    PORT: joi.number().required(),
+  })
+  .unknown(true);
+
+const { error, value } = envsSchema.validate(process.env);
+
+if (error) {
+  throw new Error(`Enviroment variables has errors: ${error.message}`);
+}
+
+const envVariables: EnvVariables = value;
 
 export const envs = {
-  port: process.env.PORT,
+  port: envVariables.PORT,
 };
