df1300b02d400aabee766a3bd2f292edae1936fc
Jeroham Sanchez <jeroham.sanchez@gmail.com>
2024-08-25 11:37:51 -0500

    S4-26: Variables de entorno configuradas

--- products-ms/src/main.ts
+++ products-ms/src/main.ts
@@ -1,6 +1,7 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { ValidationPipe } from '@nestjs/common';
+import { Logger, ValidationPipe } from '@nestjs/common';
+import { envs } from './config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -11,6 +12,7 @@ async function bootstrap() {
       forbidNonWhitelisted: true,
     }),
   );
-  await app.listen(3000);
+  await app.listen(envs.port);
+  Logger.verbose(`Products-MS is running on port: ${envs.port}`);
 }
 bootstrap();
