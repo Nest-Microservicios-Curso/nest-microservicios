451c256ca244830e28d26be66f114e8017d54da2
Jeroham Sanchez <jeroham.sanchez@gmail.com>
2024-08-25 12:05:37 -0500

    S4-27: Prisma - SQLite

--- products-ms/src/main.ts
+++ products-ms/src/main.ts
@@ -4,6 +4,8 @@ import { Logger, ValidationPipe } from '@nestjs/common';
 import { envs } from './config';
 
 async function bootstrap() {
+  const logger = new Logger('Main');
+
   const app = await NestFactory.create(AppModule);
   app.setGlobalPrefix('api');
   app.useGlobalPipes(
@@ -13,6 +15,6 @@ async function bootstrap() {
     }),
   );
   await app.listen(envs.port);
-  Logger.verbose(`Products-MS is running on port: ${envs.port}`);
+  logger.verbose(`Products-MS is running on port: ${envs.port}`);
 }
 bootstrap();
--- products-ms/src/products/products.service.ts
+++ products-ms/src/products/products.service.ts
@@ -1,9 +1,15 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, Logger, OnModuleInit } from '@nestjs/common';
 import { CreateProductDto } from './dto/create-product.dto';
 import { UpdateProductDto } from './dto/update-product.dto';
+import { PrismaClient } from '@prisma/client';
 
 @Injectable()
-export class ProductsService {
+export class ProductsService extends PrismaClient implements OnModuleInit {
+  private logger = new Logger('ProductsService');
+  onModuleInit() {
+    this.$connect();
+    this.logger.verbose('Database connected!');
+  }
   create(createProductDto: CreateProductDto) {
     return 'This action adds a new product';
   }
