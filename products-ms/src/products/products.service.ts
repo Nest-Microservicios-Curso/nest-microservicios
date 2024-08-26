1b946f58380447aeaed473b997b0fe876ec915c2
Jeroham Sanchez <jeroham.sanchez@gmail.com>
2024-08-26 15:57:23 -0500

    S4-33: eliminar producto

--- products-ms/src/products/products.service.ts
+++ products-ms/src/products/products.service.ts
@@ -48,7 +48,8 @@ export class ProductsService extends PrismaClient implements OnModuleInit {
     return await this.product.update({ where: { id }, data: updateProductDto });
   }
 
-  remove(id: number) {
-    return `This action removes a #${id} product`;
+  async remove(id: number) {
+    await this.findOne(id);
+    return await this.product.delete({ where: { id } });
   }
 }
