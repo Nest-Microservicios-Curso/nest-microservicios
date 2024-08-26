5036e68d2b734cbabd43b1442ecdbdd9c8443b95
Jeroham Sanchez <jeroham.sanchez@gmail.com>
2024-08-26 15:29:36 -0500

    S4-30: paginar mediante prisma

--- products-ms/src/products/products.service.ts
+++ products-ms/src/products/products.service.ts
@@ -2,6 +2,7 @@ import { Injectable, Logger, OnModuleInit } from '@nestjs/common';
 import { CreateProductDto } from './dto/create-product.dto';
 import { UpdateProductDto } from './dto/update-product.dto';
 import { PrismaClient } from '@prisma/client';
+import { PaginationDto } from 'src/common';
 
 @Injectable()
 export class ProductsService extends PrismaClient implements OnModuleInit {
@@ -10,12 +11,25 @@ export class ProductsService extends PrismaClient implements OnModuleInit {
     this.$connect();
     this.logger.verbose('Database connected!');
   }
-  create(createProductDto: CreateProductDto) {
-    return this.product.create({ data: createProductDto });
+  async create(createProductDto: CreateProductDto) {
+    return await this.product.create({ data: createProductDto });
   }
 
-  findAll() {
-    return this.product.findMany({});
+  async findAll(paginationDto: PaginationDto) {
+    const { page, limit } = paginationDto;
+    const totalProducts = await this.product.count();
+    const lastPage = Math.ceil(totalProducts / limit);
+    return {
+      data: await this.product.findMany({
+        skip: (page - 1) * limit,
+        take: limit,
+      }),
+      meta: {
+        totalProducts,
+        lastPage,
+        actualPage: page,
+      },
+    };
   }
 
   findOne(id: number) {
