fef9881e1fe9a631a62126083cd0918f1193052d
Jeroham Sanchez <jeroham.sanchez@gmail.com>
2024-08-27 09:45:21 -0500

    S4-36: message patterns

--- products-ms/src/products/products.service.ts
+++ products-ms/src/products/products.service.ts
@@ -48,9 +48,15 @@ export class ProductsService extends PrismaClient implements OnModuleInit {
     return product;
   }
 
-  async update(id: number, updateProductDto: UpdateProductDto) {
-    await this.findOne(id);
-    return await this.product.update({ where: { id }, data: updateProductDto });
+  async update(updateProductDto: UpdateProductDto) {
+    const { id: __, ...data } = updateProductDto;
+
+    await this.findOne(updateProductDto.id);
+
+    return await this.product.update({
+      where: { id: updateProductDto.id },
+      data: data,
+    });
   }
 
   //!! NOT RECOMMENDED: HARD DELETE
