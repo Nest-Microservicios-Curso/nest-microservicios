a6d4204c22e06d5d5ef145d8431444dac47485f7
Jeroham Sanchez <jeroham.sanchez@gmail.com>
2024-09-11 14:57:03 -0500

    S7-70: comunicando products-ms con order-ms

--- products-ms/src/products/products.service.ts
+++ products-ms/src/products/products.service.ts
@@ -83,4 +83,24 @@ export class ProductsService extends PrismaClient implements OnModuleInit {
 
     return product;
   }
+
+  async validateProduct(ids: number[]) {
+    ids = Array.from(new Set(ids));
+
+    const products = await this.product.findMany({
+      where: {
+        id: {
+          in: ids,
+        },
+      },
+    });
+
+    if (ids.length !== products.length)
+      throw new RpcException({
+        message: 'Some product were not found',
+        status: HttpStatus.BAD_REQUEST,
+      });
+
+    return products;
+  }
 }
