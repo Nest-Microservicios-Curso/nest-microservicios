e048d3933ca2cbae89338e7db488094b93e824be
Jeroham Sanchez <jeroham.sanchez@gmail.com>
2024-11-24 21:13:04 -0500

    refactor: S8-82  implemented nats server

--- products-ms/src/config/envs.ts
+++ products-ms/src/config/envs.ts
@@ -4,16 +4,21 @@ import * as joi from 'joi';
 interface EnvVars {
   PORT: number;
   DATABASE_URL: string;
+  NATS_SERVER: string[];
 }
 
 const envSchemaValidator = joi
   .object({
     PORT: joi.number().required(),
     DATABASE_URL: joi.string().required(),
+    NATS_SERVER: joi.array().items(joi.string()).required(),
   })
   .unknown(true);
 
-const { error, value } = envSchemaValidator.validate(process.env);
+const { error, value } = envSchemaValidator.validate({
+  ...process.env,
+  NATS_SERVER: process.env.NATS_SERVER?.split(','),
+});
 
 if (error) {
   throw new Error(`Config(.env) has error: ${error.message}`);
@@ -24,4 +29,5 @@ const envVars: EnvVars = value;
 export const envs = {
   port: envVars.PORT,
   dbUrl: envVars.DATABASE_URL,
+  NATS_SERVER: envVars.NATS_SERVER,
 };
